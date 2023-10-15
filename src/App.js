34851f56b3bbce85fba19a935f7984f964560d86
Bahdanovich <bahdanovich@internet.ru>
2023-10-15 22:08:57 +0300

    Learning React and Fetching Chats via API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [chats, setChats] = useState([]);
+  const [add, setAdd] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost/api/chats')
+        .then(response => response.json())
+        .then(data => {
+          setChats(Object.values(data));
+        })
+        .catch(error => {
+          console.error(error);
+        });
+  }, []);
+
+  const result = chats.map((chat, index) => {
+    return (
+        <li key={index}>
+          {chat.title}
+          <button onClick={() => handleDeleteClick(index)}>delete</button>
+        </li>
+    );
+  });
+
+  function handleDeleteClick(index) {
+    const updatedChats = [...chats];
+    updatedChats.splice(index, 1);
+    setChats(updatedChats);
+  }
+
+  function handleAddClick() {
+    if (add !== '') {
+      const newChat = {id: chats.length + 1, title: add};
+      setChats([...chats, newChat]);
+      setAdd('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <input onChange={event => setAdd(event.target.value)}/>
+        <button onClick={handleAddClick}>Add</button>
+        <ul>{result}</ul>
+      </div>
   );
 }
 
